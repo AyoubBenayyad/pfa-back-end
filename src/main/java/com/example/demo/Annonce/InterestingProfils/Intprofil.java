426e0eefd2dcfd2c44cfb5485c97848ede17e88d
Ibrahim BENZEKRI ARBAI <123778755+Ibrahim-benzekri@users.noreply.github.com>
2024-03-16 19:50:15 +0000

    Home page + search bar (users)

---
+++ src/main/java/com/example/demo/Annonce/InterestingProfils/Intprofil.java
@@ -0,0 +1,18 @@
+package com.example.demo.Annonce.InterestingProfils;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Intprofil {
+    Long id;
+    String imageUrl;
+    String fullName;
+    String email;
+}
